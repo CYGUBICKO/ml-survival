f6d3080b7495cfb8ff10a39112d6c0f99fbe8bf7
Ben Bolker <bbolker@gmail.com>
2020-02-20 15:59:16 -0500

    make seeds identical across parallel/serial

--- survival_codes.R
+++ survival_codes.R
@@ -1,19 +1,26 @@
 #### ---- Random codes for survival analysis ----
 
+## BMB: be consistent about quotes/no quotes for loading pkgs?
 library(ggplot2)
-library("pec")
-library("survival")
-library("rms")
-library("randomSurvivalForest")
-library("party")
+library(pec)
+library(survival)
+library(rms)
+library(party)
 library(dplyr)
 library(doMC)
 registerDoMC()
 
+library(randomSurvivalForest)
+## ??
+## Package ‘randomSurvivalForest’ was removed from the CRAN repository.
+## Formerly available versions can be obtained from the archive.
+## Archived on 2014-12-04 after 20 months of deprecation in favour of randomForestSRC, by the maintainer.
+## Consider using package ‘randomForestSRC’ instead. 
 
 source("funs/balPartition.R")
-
-set.seed(7902)
+## s <- 7902
+s <- 801234
+set.seed(s)
 
 #### ---- Some key functions ----
 
@@ -147,32 +154,76 @@ fitform <- (Surv(time,status) ~ age
 # )
 
 #### ---- CROSS VALIDATION ----
-ntree <- c(50, 100, 150, 200, 300)
-mtry <- seq(2, 8, 2)
+## ntree <- c(50, 100, 150, 200, 300)
+## mtry <- seq(2, 8, 2)
+ntree <- c(50,100)
+mtry <- c(2,4)
 
+## BMB: write one function that uses either %do% or %dopar% depending
+## on an argument (so that you repeat less code)
 ### In sample cross validation
 cvFunc <- function(df, ntree, mtry, nfolds = 5){
   n_folds <- nfolds
   n_train <- nrow(df)
   tuneGrid <- expand.grid(folds = 1:n_folds, ntree = ntree, mtry = mtry)
   cv_errors <- expand.grid(folds = 1:n_folds, ntree = ntree, mtry = mtry, error_rate = 0)
+  tuneGrid$seed <- -1*sample(100000,size=nrow(tuneGrid))
   folds_i <- sample(rep(1:n_folds, length.out = n_train))
   for (p in 1:nrow(tuneGrid)){
     index <- which(folds_i == tuneGrid[["folds"]][p])
     train <- df[-index, ]
     test <- df[index, ]
-    fit <- rsf(fitform, data = train, ntree = tuneGrid[["ntree"]][p], mtry = tuneGrid[["mtry"]][p])
+    fit <- rsf(fitform, data = train,
+               ntree = tuneGrid[["ntree"]][p],
+               mtry = tuneGrid[["mtry"]][p],
+               seed = tuneGrid[["seed"]][p]
+               )
     cv_errors[p, "error_rate"] <- mean(fit$err.rate)
   }
   return(cv_errors)
 }
 
+
+genFunc <- function(df, ntree, mtry, nfolds = 5, type="serial",
+                    seed=7902){
+  if (!is.null(seed)) set.seed(seed)
+  n_folds <- nfolds
+  n_train <- nrow(df)
+  tuneGrid <- expand.grid(folds = 1:n_folds, ntree = ntree, mtry = mtry)
+  cv_errors <- expand.grid(folds = 1:n_folds, ntree = ntree, mtry = mtry, error_rate = 0)
+  tuneGrid$seed <- -1*sample(100000,size=nrow(tuneGrid))
+  folds_i <- sample(rep(1:n_folds, length.out = n_train))
+  fitfun <- function(p) {
+      index <- which(folds_i == tuneGrid[["folds"]][p])
+      train <- df[-index, ]
+      test <- df[index, ]
+      fit <- rsf(fitform, data = train,
+                 ntree = tuneGrid[["ntree"]][p],
+                 mtry = tuneGrid[["mtry"]][p],
+                 seed = tuneGrid[["seed"]][p]
+                 )
+      ret <- list(folds = tuneGrid[["folds"]][p],
+                   ntree = tuneGrid[["ntree"]][p],
+                   mtry = tuneGrid[["mtry"]][p],
+                   error_rate =  mean(fit$err.rate))
+      return(ret)
+  }
+  cv_errors <- if (type=="serial") {
+      foreach (p = 1:nrow(tuneGrid), .combine=bind_rows) %do%
+          fitfun(p)
+  } else {
+      foreach (p = 1:nrow(tuneGrid), .combine=bind_rows) %dopar%
+          fitfun(p)
+  }      
+  return(cv_errors)
+}
+
 starttime <- Sys.time()
-tt <- cvFunc(train_df, ntree = ntree, mtry = mtry)
+tt_serial <- genFunc(train_df, ntree = ntree, mtry = mtry)
 endtime <- Sys.time()
 cv_time <- difftime(endtime, starttime)
 
-t2 <- (tt
+t2 <- (tt_serial
   %>% group_by(ntree, mtry)
   %>% summarise(mean_error = mean(error_rate))
   %>% mutate(type = paste0("N (", as.numeric(round(cv_time, 2)), ")"))
@@ -198,12 +249,15 @@ pcvFunc <- function(df, ntree, mtry, nfolds = 5){
   }
 }
 
+set.seed(s)
+
 starttime <- Sys.time()
-tt <- pcvFunc(train_df, ntree = ntree, mtry = mtry)
+tt_parallel <- genFunc(train_df, ntree = ntree, mtry = mtry,
+                       type="parallel")
 endtime <- Sys.time()
 pcv_time <- difftime(endtime, starttime)
 
-t3 <- (tt
+t3 <- (tt_parallel
   %>% group_by(ntree, mtry)
   %>% summarise(mean_error = mean(error_rate))
   %>% mutate(type = paste0("P (", as.numeric(round(pcv_time, 2)), ")"))
@@ -222,3 +276,16 @@ p1 <- (ggplot(dd, aes(x = as.factor(ntree), y = mean_error, group = as.factor(mt
 )
 print(p1)
 ggsave("compare_cv_parallel_cv.pdf", p1)
+
+## BMB plotting code
+tt_comb <- bind_rows(serial=tt_serial, parallel=tt_parallel, .id="type")
+p2 <- (ggplot(tt_comb, aes(x = as.factor(ntree),
+                      y = error_rate, colour = type))
+  + stat_summary(fun.data = "mean_cl_normal",position=position_dodge(width=0.25))
+    ## + geom_line()
+  + facet_wrap(~mtry)
+  + labs(x = "No. of trees", y = "Mean error rate", colour = "No. predictors\n in each split")
+  + theme_bw()
+  + theme(legend.position = "bottom")
+)
+print(p2)
